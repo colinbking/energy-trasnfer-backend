9cb270d61206daf932eae2800345f18b84557b25
Andrew Feng <andrewsfeng@gmail.com>
2020-09-20 02:55:26 -0500

    finished date

--- energyChallenge.py
+++ energyChallenge.py
@@ -1,6 +1,7 @@
 import csv
 import math
 import datetime
+from datetime import datetime
 
 mapping = {}
 dates = []
@@ -10,24 +11,11 @@ def readRow(row):
     c4 = math.inf if row[5] == '-' else math.ceil(int(row[5])/30000)
     loadTime = min(c3, c4)
     mapping[custID] = loadTime
-    start = row[6]
-    end = row[7]
-    print(start)
-    print(reorderDate(start))
-
-def reorderDate(date):
-    newDate = "20"
-    curr = 0
-    start = 0
-    while(curr < len(date)):
-        
-        curr = curr + 1
-        if(date[len(date)-curr] == '/'):
-            newDate = newDate + date[len(date)-curr+1:len(date)-start]
-            start = curr
-            newDate = newDate + '-'
-    return newDate
-
+    start = row[6] + "20"
+    end = row[7] + "20"
+    dateStart = datetime.strptime(start, "%m/%d/%Y")
+    dateEnd = datetime.strptime(end, "%m/%d/%Y")
+    dates.append((custID, dateStart, dateEnd))
 
 with open('energyTransfer.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar="|")
