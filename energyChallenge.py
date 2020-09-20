c87a7c00b4f71f29510d4101a288b56122d40a17
Andrew Feng <andrewsfeng@gmail.com>
2020-09-20 02:22:16 -0500

    datetime

--- energyChallenge.py
+++ energyChallenge.py
@@ -1,21 +1,21 @@
 import csv
 import math
-import 
+import datetime
 
 mapping = {}
+dates = []
 def readRow(row):
     custID = int(row[0])
-    print(custID)
-    c3 = 0 if row[4] == '-' else math.ceil(int(row[4])/30000)
-    c4 = 0 if row[5] == '-' else math.ceil(int(row[5])/30000)
-    loadTime = min(row[4], row[5])
-    print(loadTime)
+    c3 = math.inf if row[4] == '-' else math.ceil(int(row[4])/30000)
+    c4 = math.inf if row[5] == '-' else math.ceil(int(row[5])/30000)
+    loadTime = min(c3, c4)
     mapping[custID] = loadTime
+    start = row[6]
+    end = row[7]
+    print(start, end)
 
 with open('energyTransfer.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar="|")
     headers = next(reader, None)
     for row in reader:
         readRow(row)
-
-print(mapping)
